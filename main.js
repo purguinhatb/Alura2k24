e3769d7b0f199df8371ca4e6e18a5e27886b3a02
purguinhatb <106619423+purguinhatb@users.noreply.github.com>
2024-04-26 09:10:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -35,15 +35,19 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     if(tempoFinal > 0){
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+        return [dias,horas,minutos,seundos];
     } else {
-        return "Prazo Finalizado";
+        return [0,0,0,0];
     }
   }
 
 function atualizaCronometro(){
+    document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[1])[3];
     for (let i=0; i<contadores.lenght;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);
+        //contadores[i].textContent = calculaTempo(tempos[i]);
     }
 }
 
@@ -52,4 +56,4 @@ function comecaCronometro(){
     setInterval(atualizaCronometro,1000);
 }
 
-//comecaCronometro();
+comecaCronometro();
